f3f82987a22b49e7695e589db077578a680e9119
vkotharu <152676420+vkotharu@users.noreply.github.com>
2023-12-03 18:55:33 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
